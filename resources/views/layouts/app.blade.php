d7f5bb080efaafcf3649b26da1d08c609632ce98
Sven-Koene <32331946+Sven-Koene@users.noreply.github.com>
2018-10-08 13:50:23 +0200

    personal pages
    personal dashoards

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,7 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
+   
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
@@ -29,5 +27,12 @@
             </div>
         </main>
     </div>
+ 
+ <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+    <script>
+    CKEDITOR.replace( 'article-ckeditor' );
+    </script>
 </body>
 </html>
